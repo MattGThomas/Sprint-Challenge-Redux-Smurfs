467f2c41f54ce6dff8b5a3824fcb36dd139e7764
MattGThomas <matthewgthomas0616@gmail.com>
2019-07-21 18:28:02 -0500

    init commit

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,13 @@
+import axios from 'axios'
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const ADD_SMURF = 'ADD_SMURF'
+
+export const GETTING_SMURFS = 'GETTING_SMURFS'
+export const GET_SMURFS_COMPLETE = ' GET_SMURFS_COMPLETE' 
+export const GET_SMURFS_FAILED = 'GET_SMURFS_FAILED'
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +19,16 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export function getSmurfs() {
+  return (dispatch) => {
+    dispatch({ type: GETTING_SMURFS })
+
+    axios.get('http://localhost:3333/smurfs')
+      .then((res) => {
+        dispatch({ type: GET_SMURFS_COMPLETE, payload: res.data })
+      })
+      .catch((err) => {
+        dispatch({ type: GET_SMURFS_FAILED, payload: err.response.data })
+      })
+  }
+}
