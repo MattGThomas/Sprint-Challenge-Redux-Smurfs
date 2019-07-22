6c3e8c82f62999629b8348a07ca75936611aff85
MattGThomas <matthewgthomas0616@gmail.com>
2019-07-21 19:14:10 -0500

    added smurf form

---
+++ smurfs/src/components/AddSmurfForm.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { connect } from 'redux'
+import { newSmurf } from '../actions'
+
+class AddSmurfForm extends Component {
+    state = {
+        name: '',
+        age: '',
+        height: ''
+    }
+    changeHandler = (evt) => {
+        evt.preventDefault()
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+    submitHandler = (evt) => {
+        evt.preventDefault()
+        const newSmurf = this.state
+        this.props.newSmurf(newSmurf)
+            this.setState({
+                name: '',
+                age: '',
+                height: ''
+            })
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.submitHandler}>
+                    <input
+                        type='text'
+                        name='name'
+                        placeholder='Name'
+                        value={this.state.name}
+                        onChange={this.changeHandler}
+                    />
+                    <input
+                        type='text'
+                        name='age'
+                        placeholder='Age'
+                        value={this.state.age}
+                        onChange={this.changeHandler}
+                    />
+                    <input
+                        type='text'
+                        name='height'
+                        placeholder='height'
+                        value={this.state.height}
+                        onChange={this.changeHandler}
+                    />
+                    <button type='submit'>Add Smurf</button>
+                </form>
+            </div>
+        )
+    }
+}
